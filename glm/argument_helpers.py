ae3fd6a732fc78c0a8550aa3982ec68db7ce29ce
Simon Andersson <simon@hiddencode.me>
2016-08-30 20:43:33 +0200

    Made it possible to store functions in argument helper class

--- glm/argument_helpers.py
+++ glm/argument_helpers.py
@@ -4,22 +4,31 @@ from . import utils
 class Argument(dict):
 
     def __init__(self, **kwargs):
-        keys = [x for x in dir(self) if not callable(getattr(self, x))
-                                        and not x.startswith('__')]
+        keys = [x for x in list(set(dir(self)) - set(dir(dict)))
+                if not x.startswith('__')]
+
         for key in keys:
-            self[key] = getattr(self, key)
+            attr = getattr(self, key)
+            if hasattr(attr, '__call__'):
+                self[key] = attr()
+            else:
+                self[key] = attr
 
         self.update(kwargs)
 
 
+
 class RepoArg(Argument):
     metavar = '<username/repo>'
-    type = utils.repository_validation
+    def type(self):
+        return utils.repository_validation
 
 
 class ColorArg(Argument):
     metavar = '<color>'
-    type = utils.color_validation
+
+    def type(self):
+        return utils.color_validation
 
 
 class ShowColorArg(Argument):
