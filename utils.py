4844451aaeb69efffa85755e1028cf93e146faf4
Simon Andersson <simon@hiddencode.me>
2016-08-30 14:28:07 +0200

    Documented utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,16 @@ import struct
 
 
 def parse_validation_error(name, error):
+    """ Parses github error and returns proper error messages
+
+    Args:
+        name: label name
+        error: error dict from github json
+
+    Returns:
+        Error message or None
+    """
+
     if error['code'] == 'already_exists':
         return '🚫  The name \'{}\' already exists.'.format(name)
     elif error['code'] == 'invalid':
@@ -11,6 +21,16 @@ def parse_validation_error(name, error):
     return None
 
 def text_color(color):
+    """ Calculates hex color to determine monochrome value and returns
+        proper text color.
+
+    Args:
+        color: string in hex
+
+    Returns:
+        String with either black or white
+    """
+
     (r, g, b) = struct.unpack('BBB', bytes.fromhex(color))
     a = 1 - (0.299 * r + 0.587 * g + 0.114 * b) / 255
     if a > 0.5:
@@ -18,6 +38,15 @@ def text_color(color):
     return 'black'
 
 def color_validation(value):
+    """ Color validation for argument value.
+
+    Raises Argument error or returns the passed value.
+
+    Returns:
+        The unmodified passed value
+
+    """
+
     if len(value) != 6:
         raise argparse.ArgumentTypeError('Color must be 6 characters long'
                                          'without # or 0x')
